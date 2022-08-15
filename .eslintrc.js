df4ed42cdffe0eef95df00b9c4327fb64ad461cd
lukelliot <lucaselliotchin@gmail.com>
2022-08-14 22:33:52 -0400

    feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+	"env": {
+		"es2021": true,
+    "node": true,
+		"commonjs": true,
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended"
+	],
+	"overrides": [
+	],
+	"parser": "@typescript-eslint/parser",
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"plugins": [
+		"@typescript-eslint"
+	],
+	"rules": {
+	}
+}
